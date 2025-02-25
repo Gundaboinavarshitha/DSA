88c18921324bb92b7cea490d460496e191bad3b2
Gundaboinavarshitha <gundaboinavarsha@gmail.com>
2025-02-25 15:43:51 +0530

    Add files via upload

---
+++ lcm.py
@@ -0,0 +1,10 @@
+def gcd(a,b):
+    if b==0:
+        return a
+    return gcd(b,a%b)
+
+class Solution:
+    def lcmAndGcd(self, a : int, b : int) -> list[int]:
+        g=gcd(a,b)
+        l=(a*b)//g
+        return [l,g]
